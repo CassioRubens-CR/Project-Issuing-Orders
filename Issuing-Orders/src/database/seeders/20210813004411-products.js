dae0c91a9cb36573d60a62b698cce973d7c6e5ff
Cássio Rubens <cassiorubensadv@gmail.com>
2021-08-16 20:00:04 -0300

    FIX: update seeders products

--- Issuing-Orders/src/database/seeders/20210813004411-products.js
+++ Issuing-Orders/src/database/seeders/20210813004411-products.js
@@ -5,7 +5,7 @@ module.exports = {
     await queryInterface.bulkInsert('products',
     [
       {
-        name: 'Millenium​ Falcon',
+        name: 'Millenium Falcon',
         unit_price: 550000,
         multiple: 1,
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
@@ -19,14 +19,14 @@ module.exports = {
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
-        name: 'Super​ ​Star​ ​Destroyer',
+        name: 'Super Star Destroyer',
         unit_price: 4570000,
         multiple: 1,
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
-        name: 'TIE​ ​Fighter',
+        name: 'TIE Fighter',
         unit_price: 75000,
         multiple: 2,
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
@@ -40,14 +40,14 @@ module.exports = {
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
-        name: 'DLT-19​ ​Heavy​ ​Blaster​ ​Rifle',
+        name: 'DLT-19 Heavy Blaster Rifle',
         unit_price: 5800.,
         multiple: 1,
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
-        name: 'DL-44​ ​Heavy​ ​Blaster​ ​Pistol',
+        name: 'DL-44 Heavy Blaster Pistol',
         unit_price: 1500.00,
         multiple: 10,
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
