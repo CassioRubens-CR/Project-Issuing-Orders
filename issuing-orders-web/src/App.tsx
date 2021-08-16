d9e16b853d2da34a6971b6182319fcfda812f2ae
Cássio Rubens <cassiorubensadv@gmail.com>
2021-08-15 21:10:42 -0300

    FEAT: update config file app route and create and config service api axios

--- issuing-orders-web/src/App.tsx
+++ issuing-orders-web/src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from "./pages/Home";
+import Layout from './components/Layout/Layout';
+import { routes } from './Config';
+import RouteItem from './model/RouteItemModel';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Layout>
+          {routes.map((route: RouteItem) => (
+            <Route
+              key={`${route.key}`}
+              path={`${route.path}`}
+              component={route.component}
+              exact
+            />
+          ))}
+        </Layout>
       </Switch>
     </BrowserRouter>
   );
